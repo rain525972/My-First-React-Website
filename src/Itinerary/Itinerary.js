734967a62d2a428e58a1a8da970970cf3e758c8d
rain525972 <rain90920@gmail.com>
2023-11-03 16:39:50 +0800

    feat:itinerary connect backend

--- src/Itinerary/Itinerary.js
+++ src/Itinerary/Itinerary.js
@@ -1,45 +1,114 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './itinerary.css';
 import { Badge, Calendar, Modal, Form, Input, Button, Switch,Select } from 'antd';
 import {GrSubtractCircle} from 'react-icons/gr'
+import axios from 'axios';
 
-const initialDataList = [
-  {
-    date: 8,
-    data: [
-      {
-        type: 'success',
-        content: 'This is usual event.',
-      },
-    ],
-  },
-  {
-    date: 10,
-    data: [
-      {
-        type: 'success',
-        content: 'This is usual event.',
-      },
-      {
-        type: 'error',
-        content: 'This is error event.',
-      },
-    ],
-  },
-];
+// const initialDataList = [
+//   {
+//     year: 2021,
+//     month: 9,
+//     date: 8,
+//     data: [
+//       {
+//         type: 'success',
+//         content: 'This is usual event.',
+//       },
+//     ],
+//   },
+//   {
+//     year: 2021,
+//     month: 9,
+//     date: 10,
+//     data: [
+//       {
+//         type: 'success',
+//         content: 'This is usual event.',
+//       },
+//       {
+//         type: 'error',
+//         content: 'This is error event.',
+//       },
+//     ],
+//   },
+//   {
+//     year: 2022,
+//     month: 3,
+//     date: 8,
+//     data: [
+//       {
+//         type: 'success',
+//         content: 'This is usual event.',
+//       },
+//     ],
+//   },
+//   {
+//     year: 2022,
+//     month: 3,
+//     date: 10,
+//     data: [
+//       {
+//         type: 'success',
+//         content: 'This is usual event.',
+//       },
+//       {
+//         type: 'error',
+//         content: 'This is error event.',
+//       },
+//     ],
+//   },
+// ];
+
+
 
 export default function Itinerary() {
-  const [dataList, setDataList] = useState(initialDataList);//全部數據
+  const [dataList, setDataList] = useState([]);//全部數據
   const [selectedDate, setSelectedDate] = useState(null);//選定單個日期的數據(或可以這樣寫const [selectedDate, setSelectedDate] = useState({ date: null, data: [] });)
   const [modalVisible, setModalVisible] = useState(false);//視窗是否顯示
   // const [forceRefresh, setForceRefresh] = useState(false);//是否強制重新渲染
   const [form] = Form.useForm();//這裡的[form]裡的form跟Form組件裡的form={form}的{}裡的form一樣 
 
+  useEffect(() => {
+    const authToken = "eyJhbGciOiJSU0EtT0FFUC0yNTYiLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwidHlwZSI6IkpXRSIsInppcCI6IkRFRiJ9.ecNmUM_1j57qtFwKNAGe9DeEVwvQBUTEFdT5M8sASNVrqoUw5sUG1QkhEvw86Wt-WMzkxf9FCoR2VJ8oYbkstRLc8itZuzfMqyHYEnCpOoO7nPfNxHJ243NLDGT3LmdTbCAO0TKHHZA1S5iFhlrUTRfOXlJWkLyE1DvWQG_l8Y_T5J-1A9QHg5Hx_bOEyKVBQ0nqr8-yXJbGyp6NrG8WtPhfb1T9M_pT3Kx0wjKTMYpFoYz1stQkZtb-tT0OFMLuLyXeM1sKmqd5EqJr8b8NmIJBDtFWDki9F1R-oFXMqg3AkczTQBnhRgysh6qLtT5nwQaNhdaSOa4kXCKRRvQCcQ.hr4FiWpNlipejVypbj_-dw.pXmqKasbNyFwccDAcxEvvoZjbwqWIVnkFf8xfiCGFIgXWso9C-FjbetVrRmsZWc-nxYNh9k6eAtEtQJJ7wJyN144yaqIb_knMS4Tvtdv-GAG32VtOIKcvbVJfIOTRXq_Qg603rtf8lVsPUzng6wSC744bdKjq9AcVLXnvY7hiACvydyIEy7xvPkTjOc2YVfK.MTAG2MFMySHKEiDUvroyBc9Um1e74p2NXpFbg372HLA";
+    localStorage.setItem("authToken", authToken);
+    const storedAuthToken = localStorage.getItem("authToken");
+  
+    axios.get('http://localhost:8080/mom/web/v1.0/account?limit=20&page=1', {
+      headers: {
+        'Authorization': storedAuthToken
+      }
+    })
+    .then(response => {
+      console.log(response.data);
+      const accounts = response.data.body.accounts;
+      accounts.forEach(account => {
+        Object.keys(account).forEach(key => {
+          // Do something with the account data here
+          console.log(`${key}: ${account[key]}`);
+        });
+      });
+      const updatedDataList = response.data.body.accounts.map(account => ({
+        year: account.year,
+        month: account.month,
+        date: account.date,
+        data: [
+          {
+            type: account.type,
+            content: account.content,
+          },
+        ],
+      }));
+      console.log(updatedDataList);
+      setDataList(updatedDataList);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }, []);
+
   const cellRender = (current) => {
-    console.log("1:",current.date());
-    const listData = dataList.find((item) => item.date === current.date());
+    const listData = dataList.find((item) => item.year === current.year() && item.month === current.month() + 1 && item.date === current.date());
     if (!listData) return null;
-    console.log("2:",current.date());
     return (
       <div>
         {listData.data.map((item, index) => (
@@ -77,13 +146,12 @@ export default function Itinerary() {
   };
 
   const onSelect = (date, { source }) => {
-    let selected = dataList.find((item) => item.date === date.date());
+    let selected = dataList.find((item) => item.year === date.year() && item.month === date.month() + 1 && item.date === date.date());
     if (!selected) {
-      selected = { date: date.date(), data: [] }; // 如果没有找到匹配的数据，创建一个空对象
+      selected = { year: date.year(), month: date.month() + 1, date: date.date(), data: [] }; // 如果没有找到匹配的数据，创建一个空对象
     }
     setSelectedDate(selected);
     setModalVisible(true);
-    
   };
 
   const handleDelete = (index) => {
